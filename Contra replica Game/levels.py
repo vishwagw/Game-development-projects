f870eb4c7d6c72c5b6994659b488f5b3166ddae1
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-06 18:20:01 +0530

    Create levels.py

---
+++ Contra replica Game/levels.py
@@ -0,0 +1,11 @@
+# creating the levels of game:
+# Placeholder for level management
+class Level:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
