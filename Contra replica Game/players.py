1c4d63277c96605df887835a5009d783fbbd9a61
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-06 18:19:14 +0530

    Create players.py

---
+++ Contra replica Game/players.py
@@ -0,0 +1,26 @@
+# creating the movements of player by controllers 
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+        self.speed = 5
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
